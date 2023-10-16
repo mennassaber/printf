8d0afe56e760c5303b2430c316156c0ea770a74d
‘mennassaber’ <mennasaber202620@gmail.com>
2023-10-16 06:14:58 -0700

    printf

--- printf.c
+++ printf.c
@@ -2,46 +2,56 @@
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
-#include "main.h"
-int _printf(const char *format, ...)
-{
-char c;
-char *str;
-va_list arg;
-unsigned int i;
-char *errormsg = "error";
-va_start(arg, format);
 
-if (*format == '\0')
-{
-	return (-1);
-	write(1, errormsg, 5);
+int _printf(const char *format, ...) {
+    char c;
+    char *str;
+    va_list arg;
+    unsigned int i;
+    char *errormsg = "error";
+    va_start(arg, format);
+
+    if (format == NULL) {
+        write(1, errormsg, strlen(errormsg));
+    } else {
+        for (i = 0; i < strlen(format); i++) {
+            if (format[i] == '%') {
+                switch (format[i + 1]) {
+                    case 's':
+                        str = va_arg(arg, char*);
+                        write(1, str, strlen(str));
+                        break;
+                    case 'c':
+                        c = va_arg(arg, int);
+                        write(1, &c, 1);
+                        break;
+                    case 'i':
+                        str = va_arg(arg, char*);
+                        write(1, str, strlen(str));
+                        break;
+                    case '%':
+                        c = '%';
+                        write(1, &c, 1);
+                        break;
+                    case 'd':
+                        str = va_arg(arg, char*);
+                        write(1, str, strlen(str));
+                        break;
+                    default:
+                        break;
+                }
+                i++; // Skip the next character after the format specifier
+            } else {
+                putchar(format[i]);
+            }
+        }
+    }
+
+    va_end(arg);
+    return 0;
 }
-else
-{for(i = 0; i < strlen(format); i++)
-{if((format[i] != '%') && (format[i] != '\0'))
-{
-putchar(format[i]);}
-}
-if(format[i] == '%')
-{
-switch(format[i + 1]){
-case 's':
-str=va_arg(arg, char*);
-write(1, str, strlen(str));
-break;
-case 'c':
-c=va_arg(arg, int);
-write(1, &c, 1);
-break;
-case '%':
-c = '%';
-write(1, &c, 1);
-break;
-default:
-break;
-}
-}
-}
-return (0);
+
+int main() {
+    _printf("String:[%s] character [%c]\n", "I am a string !", 'h');
+    return 0;
 }
