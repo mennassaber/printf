d16f29ac246e71d23e05bf0bc536e9c1989e7756
rahma korish <rahmakorish@gmail.com>
2023-10-17 08:28:29 -0700

    update int

--- test.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-int main(){
-	_printf("Let's try to printf a simple sentence.\n");
-	_printf("Character:[%c]\n", 'S');
-	_printf("Character:[%c] in side a sentence\n", 'H');
-	_printf("Cast:[%c] in side a sentence\n", 899);
-	_printf("Negative:[%d]\n", -762534);
- _printf("Length:[%d, %i]\n", -99, 7);
-	return (0);}
