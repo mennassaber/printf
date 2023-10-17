33ff2beba6394b3913e01e204218d725e01ee37f
‘mennassaber’ <mennasaber202620@gmail.com>
2023-10-17 11:22:16 -0700

    int

--- test/test1.c
+++ test/test1.c
@@ -1,11 +1,8 @@
-#include <limits.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
+ *main - Entry point
+ *Return: Always 0
  */
 int main(void)
 {
---
+++ test/test2.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main-Entry point
+ * Return: Alway 0;
+ **/
+int main()
+{
+_printf("%i\n", -9783);
+_printf("%d\n", -3332);
+return (0);
+}
